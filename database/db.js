3f008c64ea313ff918916daab62f6b19f8ad8640
Akihe <aki.helin@tuni.fi>
2022-01-05 15:47:27 +0200

    Create mysql connectionpool

--- database/db.js
+++ database/db.js
@@ -1,1 +1,9 @@
-//Database functions here
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+});
