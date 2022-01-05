7771d80f2e34a37cf886aaaf49531af6a99790db
Akihe <aki.helin@tuni.fi>
2022-01-05 19:40:54 +0200

    Connect to /locations using express, testing purposes

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
-const pool = require("./database/db.js");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const pool = require("./database/db.js");
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static("frontend"));
+
+const server = app.listen(8080, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
+
+app.get("/locations", async (req, res) => {
+  console.log("noutaa locations");
+  res.send(await pool.test());
+});
 
 pool.test();
 
