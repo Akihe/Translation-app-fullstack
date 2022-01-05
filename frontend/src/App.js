b0360bfc4699dd4379f9ec3843e5a9fdc58c3de2
Akihe <aki.helin@tuni.fi>
2022-01-05 19:49:14 +0200

    Modify app.js, remove default create-react-app code and create function fetchButton, fetch and log data from /locations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+async function fetchButton() {
+  let data = await fetch("http://localhost:8080/locations");
+  let js = await data.json();
+  console.log(js);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchButton}>Nouda</button>
     </div>
   );
 }
