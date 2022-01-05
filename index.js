1bb2758d3feb0c0027fbd7bd27eb63b66ceb769d
Akihe <aki.helin@tuni.fi>
2022-01-05 17:22:53 +0200

    Add requirements express, cors

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+require("dotenv").config();
+const pool = require("./database/db.js");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("frontend/build"));
+
+pool.test();
+
 function main() {
   console.log("Hello world");
 }
