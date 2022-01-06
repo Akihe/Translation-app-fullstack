cfb584e24c519a27946e5b191e40fecfcf2e2c7e
Akihe <aki.helin@tuni.fi>
2022-01-06 12:42:06 +0200

    create variable allQuestions, include it in return

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,29 @@
 import "./App.css";
-
-async function fetchButton() {
-  let data = await fetch("http://localhost:8080/locations");
-  let js = await data.json();
-  console.log(js);
-}
+import React, { useState } from "react";
+import TranslationComponent from "./TranslationComponent";
 
 function App() {
+  const [database, setDatabase] = useState([]);
+
+  async function fetchButton() {
+    let data = await fetch("http://localhost:8080/locations");
+    let js = await data.json();
+    setDatabase(js);
+  }
+
+  const allQuestions = database.map((question) => {
+    return (
+      <TranslationComponent
+        originalWord={question.latitude}
+        correctTranslation={question.longitude}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <button onClick={fetchButton}>Nouda</button>
+      {allQuestions}
     </div>
   );
 }
