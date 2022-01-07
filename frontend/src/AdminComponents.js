e0efec95fe2567d3d6b7ece058a6a99cbc7461a9
Akihe <aki.helin@tuni.fi>
2022-01-07 14:08:34 +0200

    Create file AdminComponents.js

---
+++ frontend/src/AdminComponents.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+
+function AdminComponents() {
+  return <div></div>;
+}
+
+export default AdminComponents;
