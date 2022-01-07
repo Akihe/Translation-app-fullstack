0749b41891ab28c0ac500e35818ad5ccc6b636ec
Akihe <aki.helin@tuni.fi>
2022-01-07 14:14:48 +0200

    Create new file AdminPage.js, calls AdminComponents.js

---
+++ frontend/src/AdminPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminComponents from "./AdminComponents";
+
+function AdminPage() {
+  return (
+    <div>
+      <p>Tänne tulee admin setit</p>
+      <AdminComponents />
+    </div>
+  );
+}
+
+export default AdminPage;
