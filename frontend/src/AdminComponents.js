7d5f76c4ebebb2cadf4ab46dff138c89ec383a1d
Akihe <aki.helin@tuni.fi>
2022-01-07 14:12:48 +0200

    Modify AdminComponents.js to have 3 input fields, will be used to create a new row in database

--- frontend/src/AdminComponents.js
+++ frontend/src/AdminComponents.js
@@ -3,7 +3,51 @@ import Input from "@mui/material/Input";
 import Button from "@mui/material/Button";
 
 function AdminComponents() {
-  return <div></div>;
+  const [finnishWord, setFinnishWord] = useState();
+  const [englishWord, setEnglishWord] = useState();
+  const [tag, setTag] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  function handleChange(input, target) {
+    if (target.equals("finnish")) {
+      setFinnishWord(input.target.value);
+    } else if (target.equals("english")) {
+      setEnglishWord(input.target.value);
+    } else if (target.equals("tag")) {
+      setTag(input.target.value);
+    }
+  }
+
+  return (
+    <div>
+      <div className="submitNew">
+        <form onSubmit={handleSubmit}>
+          <Input
+            value={finnishWord}
+            onChange={(input) => handleChange(input, "finnish")}
+            placeholder="In Finnish"
+          ></Input>
+          <Input
+            value={englishWord}
+            onChange={(input) => handleChange(input, "english")}
+            placeholder="In English"
+          ></Input>
+          <Input
+            value={tag}
+            onChange={(input) => handleChange(input, "tag")}
+            placeholder="tag"
+          ></Input>
+
+          <Button variant="contained" type="submit">
+            Update
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default AdminComponents;
