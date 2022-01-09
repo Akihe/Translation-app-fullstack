8671deede9e2c7e43ad5332e02dbbe24c3214a2e
Akihe <aki.helin@tuni.fi>
2022-01-09 18:15:45 +0200

    Create function postToDatabase, variables finnishWord, englishWord and tag. Posts data to database

--- frontend/src/AdminComponents.js
+++ frontend/src/AdminComponents.js
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
 import Input from "@mui/material/Input";
 import Button from "@mui/material/Button";
+import TranslationComponent from "./TranslationComponent";
 
 function AdminComponents() {
-  const [finnishWord, setFinnishWord] = useState();
-  const [englishWord, setEnglishWord] = useState();
-  const [tag, setTag] = useState();
+  const [finnishWord, setFinnishWord] = useState("");
+  const [englishWord, setEnglishWord] = useState("");
+  const [tag, setTag] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
+    postToDatabase();
   }
 
   function handleChange(input, target) {
-    if (target.equals("finnish")) {
+    if (target === "finnish") {
       setFinnishWord(input.target.value);
-    } else if (target.equals("english")) {
+    } else if (target === "english") {
       setEnglishWord(input.target.value);
-    } else if (target.equals("tag")) {
+    } else if (target === "tag") {
       setTag(input.target.value);
     }
+  }
+
+  function postToDatabase() {
+    fetch("http://localhost:8080/dictionary/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        word_in_finnish: finnishWord,
+        word_in_english: englishWord,
+        tag: tag,
+      }),
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -42,7 +61,7 @@ function AdminComponents() {
           ></Input>
 
           <Button variant="contained" type="submit">
-            Update
+            Submit
           </Button>
         </form>
       </div>
