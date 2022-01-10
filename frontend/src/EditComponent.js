00285751d520976ed907fc60d3354f4b0a3c3b88
Akihe <aki.helin@tuni.fi>
2022-01-10 14:35:26 +0200

    create handleClickOpen(), calls file EditPopup. possibly not final type of implementation

--- frontend/src/EditComponent.js
+++ frontend/src/EditComponent.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditPopup from "./EditPopup";
 
 function EditComponent({
   id,
@@ -8,14 +9,27 @@ function EditComponent({
   correctTranslation,
   tag,
   deleteQuestion,
+  editQuestion,
 }) {
+  const handleClickOpen = () => {
+    <EditPopup
+      id={id}
+      originalWord={originalWord}
+      correctTranslation={correctTranslation}
+      tag={tag}
+      editQuestion={tag}
+    />;
+  };
+
   return (
     <div className="flexContent">
       <p>Id = {id}</p>
       <p>Finnish = {originalWord}</p>
       <p>English = {correctTranslation}</p>
       <p>Tag = {tag}</p>
-      <Button variant="outlined">Edit</Button>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Edit
+      </Button>
       <Button
         variant="outlined"
         startIcon={<DeleteIcon />}
