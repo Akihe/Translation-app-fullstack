156a8d028b4b6e4af997f5a5bffffa6cd0e98ef8
Akihe <aki.helin@tuni.fi>
2022-01-11 19:38:27 +0200

    Edit UserPage.js, add button to select which category you wish to play on. Add possibility to choose between playing finnish to english or vice versa.

--- frontend/src/UserPage.js
+++ frontend/src/UserPage.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
 import TranslationComponent from "./TranslationComponent";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
+import { Button } from "@mui/material";
 
 function UserPage() {
   const [database, setDatabase] = useState([]);
+  const [selectedTag, setSelectedTags] = useState([]);
+  const [chosenQuestions, setChosenQuestions] = useState([]);
 
   async function fetchAll() {
     let data = await fetch("http://localhost:8080/dictionary");
@@ -15,20 +25,90 @@ function UserPage() {
     fetchAll();
   }, []);
 
-  const allQuestions = database.map((question, index) => {
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const categories = [
+    ...new Set(
+      database.map((question) => {
+        return question.tag;
+      })
+    ),
+  ];
+
+  function MultipleSelectCheckmarks() {
+    const handleChange = (event) => {
+      const {
+        target: { value },
+      } = event;
+      setSelectedTags(
+        // On autofill we get a stringified value.
+        typeof value === "string" ? value.split(",") : value
+      );
+    };
     return (
-      <TranslationComponent
-        key={index}
-        originalWord={question.word_in_finnish}
-        correctTranslation={question.word_in_english}
-      />
+      <div>
+        <FormControl sx={{ m: 1, width: 300 }}>
+          <InputLabel id="demo-multiple-checkbox-label">Tag</InputLabel>
+          <Select
+            labelId="demo-multiple-checkbox-label"
+            id="demo-multiple-checkbox"
+            multiple
+            value={selectedTag}
+            onChange={handleChange}
+            input={<OutlinedInput label="Tag" />}
+            renderValue={(selected) => selected.join(", ")}
+            MenuProps={MenuProps}
+          >
+            {categories.map((tag) => (
+              <MenuItem key={tag} value={tag}>
+                <Checkbox checked={selectedTag.indexOf(tag) > -1} />
+                <ListItemText primary={tag} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     );
-  });
+  }
+
+  function selectLanguage(finnishToEnglish) {
+    const chooseQuestions = database.map((question, index) => {
+      if (selectedTag.includes(question.tag)) {
+        return (
+          <TranslationComponent
+            key={index}
+            originalWord={question.word_in_finnish}
+            correctTranslation={question.word_in_english}
+            finnishToEnglish={finnishToEnglish}
+          />
+        );
+      } else {
+        return null;
+      }
+    });
+    setChosenQuestions(chooseQuestions);
+  }
 
   return (
     <div>
       <h1>This is where you play the game</h1>
-      {allQuestions}
+      <Button variant="outlined" onClick={() => selectLanguage(true)}>
+        Play Finnish to English
+      </Button>
+      <Button variant="outlined" onClick={() => selectLanguage(false)}>
+        Play English to Finnish
+      </Button>
+      <MultipleSelectCheckmarks />
+      {chosenQuestions}
     </div>
   );
 }
