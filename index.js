799ed31cf8bb42648b4f15d25840777429adad8b
Akihe <aki.helin@tuni.fi>
2022-01-14 01:11:15 +0200

    attempting to fix heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
 
 const pool = require("./database/db.js");
@@ -13,6 +15,13 @@ const port = process.env.PORT || 8080;
 
 const server = app.listen(port, () => {
   console.log(`Listening on port ${server.address().port}`);
+});
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "frontend/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
 });
 
 app.get("/dictionary", async (req, res) => {
